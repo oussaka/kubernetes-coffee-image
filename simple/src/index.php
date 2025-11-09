0d469d21d46bc76a19cc3ca1f018c302f82a011d
oussaka <kaabachi.dev@gmail.com>
2025-11-09 12:19:18 +0100

    auto sync update content (2) test

--- simple/src/index.php
+++ simple/src/index.php
@@ -51,6 +51,7 @@
                     <?php echo gethostname(); ?>
                 </p>
                 <p>Content: Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                <p>Content: Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
             </div>
         </footer>
     </center>
